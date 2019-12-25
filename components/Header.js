b5254fc1b95d6ee2638c6ecbada7feefdecf4b8f
Jordan Le Pera <leperajordan@gmail.com>
2019-12-26 00:44:01 +0100

    fix build error

--- components/Header.js
+++ components/Header.js
@@ -12,6 +12,7 @@ const Header = props => {
     <>
       <Head>
         <title>{t('h1')}</title>
+        <base href="./" />
         <link data-react-helmet="true" rel="icon" href="https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/spaces%2F-L9iS6Wm2hynS5H9Gj7j%2Favatar.png?generation=1523462254548780&amp;alt=media" />
       </Head>
       <Nav />
