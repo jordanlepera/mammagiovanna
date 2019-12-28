7402f497749e70d400051b5ceb09237503d268c3
Jordan Le Pera <leperajordan@gmail.com>
2019-12-28 03:20:58 +0100

    Fix navbar for mobile

--- components/Nav.js
+++ components/Nav.js
@@ -30,13 +30,13 @@ const Nav = ({ t }) => {
   return (
     <>
       <NavBar>
-        <Toolbar>
+        <NavToolbar>
           <HeaderLogo src="/logo-txt-vector.png" alt="Mamma Giovanna's logo" />
           <NavlinkContainer>
             <Navlink />
           </NavlinkContainer>
           <SelectCountry />
-        </Toolbar>
+        </NavToolbar>
       </NavBar>
     </>
   );
@@ -52,21 +52,41 @@ Nav.propTypes = {
 
 const NavlinkContainer = styled.div`
   flex-grow: 1;
+  @media (max-width: 500px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 const NavLink = styled(Button)`
   color: white;
   margin-left: 10px;
   margin-right: 10px;
+  @media (max-width: 500px) {
+    margin-bottom: 15px;
+  }
 `;
 
 const NavBar = styled(AppBar)`
   background-color: #4c2d19;
+  @media (max-width: 500px) {
+    min-height: 150px;
+    padding: 20px;
+  }
+`;
+
+const NavToolbar = styled(Toolbar)`
+  @media (max-width: 500px) {
+    flex-direction: column;
+  }
 `;
 
 const HeaderLogo = styled.img`
   width: 75px;
-  margin-right: 20px;
+  margin: 10px;
+  @media (min-width: 501px) {
+    margin-right: 20px;
+  }
 `;
 
 export default withTranslation('common')(Nav);
