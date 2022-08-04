fba6fd356a4403210ea8aab507a66e4e3caecf1f
jordanlepera <leperajordan@gmail.com>
2022-08-04 15:25:18 +0200

    Restore previous buid command

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 test "$CI" = true || exit 1
-npm run build && npx pnpm install -r --store-dir=node_modules/.pnpm-store
+npx pnpm install -r --store-dir=node_modules/.pnpm-store
 # other build scripts
