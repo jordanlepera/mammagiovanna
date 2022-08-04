f9a18c90fed80fd0fd5c917ed63ef03235fb5892
jordanlepera <leperajordan@gmail.com>
2022-08-04 15:27:09 +0200

    Try another build command

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 test "$CI" = true || exit 1
-npx pnpm install -r --store-dir=node_modules/.pnpm-store
+npx pnpm install -r --store-dir=node_modules/.pnpm-store && npx pnpm run build
 # other build scripts
